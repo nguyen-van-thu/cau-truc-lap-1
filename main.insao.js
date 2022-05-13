c93959c5dfec003db2dd2dba2aa26e456019cc0f
nguyen-van-thu <102147814+nguyen-van-thu@users.noreply.github.com>
2022-05-13 09:46:09 +0700

    cau-truc-lap

---
+++ main.insao.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 9; i++) {
+    for (let j = 1; j <= 9; j++) {
+        if(i<=j){
+            document.write("*");
+        } else (i > j)
+        document.write('&nbsp'+ "<br>");
+    }
+}
